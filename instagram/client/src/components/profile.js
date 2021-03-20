5d13d6ce6f00e5b6cf36c008980943df021a9093
ausi1997.com <ausafen1018@gmail.com>
2021-03-20 16:46:22 +0530

    profile page ui

--- instagram/client/src/components/profile.js
+++ instagram/client/src/components/profile.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 const Profile = ()=>{
     return(
-        <h1>Profile</h1>
+        <div style={{margin:"auto",maxWidth:"650px"}}>
+        <div style={{display:"flex",justifyContent:"space-around",margin:"18px 0px",borderBottom:"2px solid black"}}>
+        <div>
+        <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+         src="https://images.unsplash.com/photo-1499952127939-9bbf5af6c51c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTF8fHBlcnNvbnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"></img>
+        </div>
+        <div><h4>Ausaf Ahmad</h4>
+        <div style={{display:"flex",justifyContent:"space-between",width:"115%"}}>
+        <h5>20 posts</h5>
+        <h5> 200 followers</h5>
+        <h5>250 following</h5>
+        </div>
+        </div>
+        </div>
+        <div className="gallery">
+        <img className="item" src="https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2NlbmVyeXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"></img>
+        <img className="item" src="https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2NlbmVyeXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"></img>
+        <img className="item" src="https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2NlbmVyeXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"></img>
+        <img className="item" src="https://images.unsplash.com/photo-1494783367193-149034c05e8f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8c2NlbmVyeXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60"></img>
+
+        </div>
+        </div>
     )
 }
 
