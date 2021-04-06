c73fdbbd5cf312ce17995e4094baaec919a1613a
ausi1997.com <ausafen1018@gmail.com>
2021-04-06 11:09:36 +0530

    database online hosted

--- instagram/server/models/db.js
+++ instagram/server/models/db.js
@@ -1,6 +1,8 @@
 const mongoose  = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/instagram', {useNewUrlParser:true, useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:true},(err)=>{
+const URL = 'mongodb+srv://ausi97:ausi@1997@cluster0.fhhec.mongodb.net/instagram?retryWrites=true&w=majority';
+
+mongoose.connect(URL, {useNewUrlParser:true, useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:true},(err)=>{
 // check error   
 if(err){
         console.log('DB Connection fails' +err);
